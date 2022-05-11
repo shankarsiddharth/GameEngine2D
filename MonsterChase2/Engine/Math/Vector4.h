ed92e4465df100ec0098b990338f0f79feaecdf6
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-05-11 08:19:43 -0600

    Create Vector4 header declarations

--- MonsterChase2/Engine/Math/Vector4.h
+++ MonsterChase2/Engine/Math/Vector4.h
@@ -6,6 +6,40 @@ public:
 	Vector4();
 	~Vector4();
 
-private:
+	Vector4(float InX, float InY, float InZ, float InW);
+	Vector4(const Vector4& InOther);
 
+	float X() const;
+	float Y() const;
+	float Z() const;
+	float W() const;
+
+	void X(const float InX);
+	void Y(const float InY);
+	void Z(const float InZ);
+	void W(const float InW);
+
+	Vector4 operator+(const Vector4& InVector);
+	Vector4 operator-(const Vector4& InVector);
+	Vector4 operator*(const Vector4& InVector);
+	Vector4 operator/(const Vector4& InVector);
+
+	Vector4& operator+=(const Vector4& InVector);
+	Vector4& operator-=(const Vector4& InVector);
+	
+	Vector4& operator*=(float const& InValue);
+	Vector4& operator/=(float const& InValue);
+
+	Vector4 operator / (float const& InValue);
+	Vector4 operator * (float const& InValue);
+
+	static float DotProduct(const Vector4& InVector);
+
+	static const Vector4 Zero;
+
+private:
+	float m_X;
+	float m_Y;
+	float m_Z;
+	float m_W;
 };
