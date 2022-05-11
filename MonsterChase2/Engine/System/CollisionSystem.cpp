1c40504dc30f72171af7ecfdaf58ba70a514222c
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-05-11 16:01:35 -0600

    Initialize box colliders from the game world

--- MonsterChase2/Engine/System/CollisionSystem.cpp
+++ MonsterChase2/Engine/System/CollisionSystem.cpp
@@ -1,4 +1,9 @@
 #include "CollisionSystem.h"
+#include <vector>
+#include "../SmartPointer/SmartPointer.h"
+#include "../Object/GameObject.h"
+#include "../Component/BoxCollider2D.h"
+#include "../GameWorld.h"
 
 CollisionSystem::CollisionSystem()
 {
@@ -17,7 +22,17 @@ void CollisionSystem::Initialize()
 
 void CollisionSystem::Update(float deltaTime, GameWorld* i_GameWorld)
 {
-
+	//Get All Colliders from the GameWorld
+	std::vector<BoxCollider2D*> boxColliders;
+	std::vector<SharedPointer<GameObject>> gameObjects = i_GameWorld->GetAllGameObjects();
+	for (SharedPointer<GameObject> gameObject : gameObjects)
+	{
+		BoxCollider2D* boxCollider2DComponent = gameObject->GetComponent<BoxCollider2D>();
+		if (boxCollider2DComponent != nullptr)
+		{
+			boxColliders.push_back(boxCollider2DComponent);
+		}
+	}
 }
 
 void CollisionSystem::ShutDown()
