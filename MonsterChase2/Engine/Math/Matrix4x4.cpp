ec0667fa8601c45b80501d41e390e5f3f25b31ce
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-05-11 08:52:08 -0600

    Create Matrix4x4 files

---
+++ MonsterChase2/Engine/Math/Matrix4x4.cpp
@@ -0,0 +1,11 @@
+#include "Matrix4x4.h"
+
+Matrix4x4::Matrix4x4()
+{
+
+}
+
+Matrix4x4::~Matrix4x4()
+{
+
+}
---
+++ MonsterChase2/Engine/Math/Matrix4x4.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Matrix4x4
+{
+public:
+	Matrix4x4();
+	~Matrix4x4();
+
+private:
+
+};
