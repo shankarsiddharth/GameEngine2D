8a9dd65842dbbdd636a144486eab37ca08a49549
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-05-11 18:18:14 -0600

    Add base logic for Separating Axis

--- MonsterChase2/Engine/System/CollisionSystem.cpp
+++ MonsterChase2/Engine/System/CollisionSystem.cpp
@@ -1,5 +1,4 @@
 #include "CollisionSystem.h"
-#include <vector>
 #include "../SmartPointer/SmartPointer.h"
 #include "../Object/GameObject.h"
 #include "../Component/BoxCollider2D.h"
@@ -32,10 +31,12 @@ void CollisionSystem::Update(float deltaTime, GameWorld* i_GameWorld)
 		{
 			//Update the collider extents based on the object position & rotation
 			boxCollider2DComponent->UpdateWorldExtents();
+			boxCollider2DComponent->UpdateWorldExtentsAxes();
 			boxColliders.push_back(boxCollider2DComponent);
 		}
 	}
 
+	SeparatingAxisCheck(boxColliders);
 
 }
 
@@ -44,3 +45,40 @@ void CollisionSystem::ShutDown()
 
 }
 
+void CollisionSystem::SeparatingAxisCheck(const std::vector<BoxCollider2D*>& InBoxColliderList)
+{
+	for (int i = 0; i < InBoxColliderList.size(); i++)
+	{
+		for (int j = 0; j < InBoxColliderList.size(); j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+
+			BoxCollider2D& colliderA = *(InBoxColliderList[i]);
+			BoxCollider2D& colliderB = *(InBoxColliderList[j]);
+
+			bool doesIntersect = DoesInterect(colliderA, colliderB) && DoesInterect(colliderB, colliderA);
+		}
+	}
+}
+
+bool CollisionSystem::DoesInterect(BoxCollider2D& A, BoxCollider2D& B)
+{
+	for (const Vector4& AAxis : A.GetWorldExtentAxes())
+	{
+		for (const Vector4& vertex : A.GetWorldExtentCoordinates()) {
+			//TODO: Dot product			
+			//TODO: Get the Min & Max
+		}
+
+		for (const Vector4& vertex : B.GetWorldExtentCoordinates()) {
+			//TODO: Dot product
+			//TODO: Get the Min & Max
+		}
+
+		//TODO: Check the min and max to determine possible intersection
+	}
+	return false;
+}
--- MonsterChase2/Engine/System/CollisionSystem.h
+++ MonsterChase2/Engine/System/CollisionSystem.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <vector>
 
 class GameWorld;
+class BoxCollider2D;
 
 class CollisionSystem
 {
@@ -12,5 +14,8 @@ public:
 	void Update(float deltaTime, GameWorld* i_GameWorld);
 	void ShutDown();
 
+private:
+	void SeparatingAxisCheck(const std::vector<BoxCollider2D*>& InBoxColliderList);
+	bool DoesInterect(BoxCollider2D& A, BoxCollider2D& B);
 };
 
