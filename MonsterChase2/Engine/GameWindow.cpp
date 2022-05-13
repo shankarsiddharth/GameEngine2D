f6d0f7ad89534f3d9edb0c184efdd1ecb3394c77
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-05-12 23:29:27 -0600

    Add methods to set the window name height & width

--- MonsterChase2/Engine/GameWindow.cpp
+++ MonsterChase2/Engine/GameWindow.cpp
@@ -16,6 +16,19 @@ bool GameWindow::Initialize(HINSTANCE i_hInstance, int i_nCmdShow,
 	unsigned int i_WindowWidth /*= DEFAULT_GAME_WINDOW_WIDTH_SIZE_X*/, 
 	unsigned int i_WindowHeight /*= DEFAULT_GAME_WINDOW_HEIGHT_SIZE_Y*/)
 {
+	if (WindowHeight != 0)
+	{
+		i_WindowHeight = WindowHeight;
+	}
+	if (WindowWidth != 0)
+	{
+		i_WindowWidth = WindowWidth;
+	}
+	if (WindowName != "")
+	{
+		i_WindowName = WindowName;
+	}
+
 	// IMPORTANT: first we need to initialize GLib
 	return GLib::Initialize(i_hInstance, i_nCmdShow, i_WindowName.c_str(), -1, i_WindowWidth, i_WindowHeight, true);
 }
@@ -35,5 +48,35 @@ void GameWindow::ShutDown()
 bool GameWindow::IsWindowClosed()
 {
 	return bIsWindowClosed;
+}
+
+std::string GameWindow::GetWindowName() const
+{
+	return WindowName;
+}
+
+void GameWindow::SetWindowName(std::string i_WindowName)
+{
+	WindowName = i_WindowName;
+}
+
+unsigned int GameWindow::GetWindowHeight() const
+{
+	return WindowHeight;
+}
+
+void GameWindow::SetWindowHeight(unsigned int i_WindowHeight)
+{
+	WindowHeight = i_WindowHeight;
+}
+
+unsigned int GameWindow::GetWindowWidth() const
+{
+	return WindowWidth;
+}
+
+void GameWindow::SetWindowWidth(unsigned int i_WindowWidth)
+{
+	WindowWidth = i_WindowWidth;
 }
 
--- MonsterChase2/Engine/GameWindow.h
+++ MonsterChase2/Engine/GameWindow.h
@@ -18,8 +18,22 @@ public:
 
 	bool IsWindowClosed();
 
+	std::string GetWindowName() const;
+	void SetWindowName(std::string i_WindowName);
+
+	unsigned int GetWindowHeight() const;
+	void SetWindowHeight(unsigned int i_WindowHeight);
+	unsigned int GetWindowWidth() const;
+	void SetWindowWidth(unsigned int i_WindowWidth);
+
 private:
 	bool bIsWindowClosed = false;
+
+	std::string WindowName = "";
+
+	unsigned int WindowHeight = 0;
+	unsigned int WindowWidth = 0;
+
 
 	static const unsigned int DEFAULT_GAME_WINDOW_WIDTH_SIZE_X = 800;
 	static const unsigned int DEFAULT_GAME_WINDOW_HEIGHT_SIZE_Y = 600;
